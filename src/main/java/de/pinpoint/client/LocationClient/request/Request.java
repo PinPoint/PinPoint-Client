2a685def6d5c32e180281ccbfdb21b01b39e7ec0
Nico Franke <nico.franke01@gmail.com>
2021-01-08 17:26:30 +0100

    :construction: Add data tranfser objects

---
+++ src/main/java/de/pinpoint/client/LocationClient/request/Request.java
@@ -0,0 +1,19 @@
+package de.pinpoint.client.LocationClient.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public abstract class Request {
+    /**
+     * The uuid of the client's user the request was sent from.
+     */
+    private UUID userId;
+
+    public Request(){};
+
+    public abstract String getPath();
+}
---
+++ src/main/java/de/pinpoint/client/LocationClient/request/UserListRequest.java
@@ -0,0 +1,18 @@
+package de.pinpoint.client.LocationClient.request;
+
+import java.util.UUID;
+
+/**
+ * This is used by the client to request a list of all active users.
+ */
+public class UserListRequest extends Request {
+    public UserListRequest(UUID userId) {
+        super(userId);
+    }
+    public UserListRequest(){}
+
+    @Override
+    public String getPath() {
+        return "list";
+    }
+}
